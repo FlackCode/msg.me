3566c14deed69ccc9199eb23ff242fec242a7912
flack <163582560+FlackCode@users.noreply.github.com>
2024-05-07 07:15:08 +0200

    Fixed text going out of notes container

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,7 @@
 const Note = ({ note }) => {
 
     return (
-      <div className="p-4 shadow-md rounded-xl border-gray-100 border-2">
+      <div className="p-4 shadow-md rounded-xl border-gray-100 border-2 break-words">
         <h1 className="font-bold text-lg">{note.name}</h1>
         <p>{note.content}</p>
       </div>
